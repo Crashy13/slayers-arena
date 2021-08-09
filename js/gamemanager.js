360a24b56250acd916bee8e3490637fc40729240
Scotty Wyatt <crashy@Scottys-MacBook-Pro.local>
2021-08-09 12:02:17 -0400

    added characters stats to the players

--- js/gamemanager.js
+++ js/gamemanager.js
@@ -4,7 +4,20 @@ let GameManager = {
     this.setPreFight();
   },
   resetPlayer: function(classType) {
-
+    switch(classType) {
+      case: "Knight":
+        player = new Player(classType, 200, 0, 200, 100, 50);
+        break;
+      case: "Archer":
+        player = new Player(classType, 200, 0, 50, 200, 100);
+        break;
+      case: "Assassin":
+        player = new Player(classType, 100, 0, 100, 150, 200);
+        break;
+      case: "Mage":
+        player = new Player(classType, 80, 0, 50, 200, 50);
+        break;
+    }
   },
   setPreFight: function() {
 
