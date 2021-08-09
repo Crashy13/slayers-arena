a5803d4ceb8f3e1239bd7733bfc481f3d6c1000b
Scotty Wyatt <crashy@Scottys-MacBook-Pro.local>
2021-08-09 14:30:23 -0400

    added attack function for player and opponent and the health changes dynamically with each attack

--- js/gamemanager.js
+++ js/gamemanager.js
@@ -9,17 +9,17 @@ let GameManager = {
         player = new Player(classType, 200, 0, 200, 100, 50);
         break;
       case "Archer":
-        player = new Player(classType, 200, 0, 50, 200, 100);
+        player = new Player(classType, 200, 0, 50, 150, 100);
         break;
       case "Assassin":
-        player = new Player(classType, 100, 0, 100, 150, 200);
+        player = new Player(classType, 100, 0, 20, 200, 200);
         break;
       case "Mage":
         player = new Player(classType, 80, 0, 50, 200, 50);
         break;
     }
     let getInterface = document.querySelector('.interface');
-    getInterface.innerHTML = '<img src="images/player/' + classType.toLowerCase() + '.jpeg" class="image-avatar"><div><h3>' + classType + '</h3><p>Health: ' + player.health + '</p><p>Magic: ' + player.magic + '</p><p>Strength: ' + player.strength + '</p><p>Agility: ' + player.agility + '</p><p>Speed: ' + player.speed + '</p></div>';
+    getInterface.innerHTML = '<img src="images/player/' + classType.toLowerCase() + '.jpeg" class="image-avatar"><div><h3>' + classType + '</h3><p class="health-player">Health: ' + player.health + '</p><p>Magic: ' + player.magic + '</p><p>Strength: ' + player.strength + '</p><p>Agility: ' + player.agility + '</p><p>Speed: ' + player.speed + '</p></div>';
   },
   setPreFight: function() {
     let getHeader = document.querySelector('.header');
@@ -33,9 +33,9 @@ let GameManager = {
     let getHeader = document.querySelector('.header');
     let getActions = document.querySelector('.actions');
     let getEnemy = document.querySelector('.enemy');
-    let enemy00 = new Enemy("Ogre", 100, 0, 50, 100, 100);
-    let enemy01 = new Enemy("Golem", 150, 0, 80, 150, 100);
-    let enemy02 = new Enemy("Dragon", 200, 0, 100, 200, 150);
+    let enemy00 = new Enemy("Ogre", 100, 0, 80, 100, 100);
+    let enemy01 = new Enemy("Golem", 150, 0, 100, 120, 100);
+    let enemy02 = new Enemy("Dragon", 200, 0, 100, 150, 150);
     let chooseRandomEnemy = Math.floor(Math.random() * Math.floor(3));
     switch(chooseRandomEnemy) {
       case 0:
@@ -50,6 +50,6 @@ let GameManager = {
     }
     getHeader.innerHTML = '<p>Task: Choose Your Move</p>';
     getActions.innerHTML = '<a href="#" class="btn-prefight" onclick="PlayerMoves.calcAttack()">Attack!</a>';
-    getEnemy.innerHTML = '<img src="images/enemy/' + enemy.enemyType.toLowerCase() + '.jpeg" class="image-avatar"><div><h3>' + enemy.enemyType + '</h3><p>Health: ' + enemy.health + '</p><p>Magic: ' + enemy.magic + '</p><p>Strength: ' + enemy.strength + '</p><p>Agility: ' + enemy.agility + '</p><p>Speed: ' + enemy.speed + '</p></div>';
+    getEnemy.innerHTML = '<img src="images/enemy/' + enemy.enemyType.toLowerCase() + '.jpeg" class="image-avatar"><div><h3>' + enemy.enemyType + '</h3><p class="health-enemy">Health: ' + enemy.health + '</p><p>Magic: ' + enemy.magic + '</p><p>Strength: ' + enemy.strength + '</p><p>Agility: ' + enemy.agility + '</p><p>Speed: ' + enemy.speed + '</p></div>';
   }
 }
--- js/player.js
+++ js/player.js
@@ -11,7 +11,86 @@ function Player(classType, health, magic, strength, agility, speed) {
 
 let PlayerMoves = {
   calcAttack: function () {
-    let getPlayerSpeed = player.speed
-    let getEnemySpeed = enemy.speed
+    let getPlayerSpeed = player.speed;
+    let getEnemySpeed = enemy.speed;
+    let playerAttack = function () {
+      let calcBaseDamage;
+      if (player.magic > 0) {
+        calcBaseDamage = player.strength * player.magic / 1000;
+      } else {
+        calcBaseDamage = player.strength * player.agility / 1000;
+      }
+      let offsetDamage = Math.floor(Math.random() * Math.floor(10));
+      let calcOutputDamage = calcBaseDamage + offsetDamage;
+      let numberOfHits = Math.floor(Math.random() * Math.floor(player.agility / 10) / 2) + 1;
+      let attackValues = [calcOutputDamage, numberOfHits];
+      return attackValues;
+    }
+    let enemyAttack = function () {
+      let calcBaseDamage;
+      if (enemy.magic > 0) {
+        calcBaseDamage = enemy.strength * enemy.magic / 1000;
+      } else {
+        calcBaseDamage = enemy.strength * enemy.agility / 1000;
+      }
+      let offsetDamage = Math.floor(Math.random() * Math.floor(10));
+      let calcOutputDamage = calcBaseDamage + offsetDamage;
+      let numberOfHits = Math.floor(Math.random() * Math.floor(enemy.agility / 10) / 2) + 1;
+      let attackValues = [calcOutputDamage, numberOfHits];
+      return attackValues;
+    }
+    let getPlayerHealth = document.querySelector('.health-player');
+    let getEnemyHealth = document.querySelector('.health-enemy');
+    // Start attack
+    if (getPlayerSpeed >= getEnemySpeed) {
+      let playerAttackValues = playerAttack();
+      let totalDamage = playerAttackValues[0] * playerAttackValues[1];
+      enemy.health = enemy.health - totalDamage;
+      alert("You hit " + playerAttackValues[1] + " times for " + playerAttackValues[0] + " damage each for a total of " + totalDamage + " total damage!");
+      if (enemy.health <= 0) {
+        alert("You are victorious! Refresh the browser to slay again!");
+        getPlayerHealth.innerHTML = 'Health: ' + player.health;
+        getEnemyHealth.innerHTML = 'Health: 0';
+      } else {
+        getEnemyHealth.innerHTML = 'Health: ' + enemy.health;
+        // Enemy attack
+        let enemyAttackValues = enemyAttack();
+        let totalDamage = enemyAttackValues[0] * enemyAttackValues[1];
+        player.health = player.health - totalDamage;
+        alert("You have been hit " + enemyAttackValues[1] + " times for " + enemyAttackValues[0] + " damage each for a total of " + totalDamage + " total damage!");
+        if (player.health <= 0) {
+          alert("You have been defeated! Refresh the browser for redemption!");
+          getPlayerHealth.innerHTML = 'Health: 0';
+          getEnemyHealth.innerHTML = 'Health: ' + enemy.health;
+        } else {
+          getPlayerHealth.innerHTML = 'Health: ' + player.health;
+        }
+      }
+    } else if (getEnemySpeed > getPlayerSpeed) {
+      let enemyAttackValues = enemyAttack();
+      let totalDamage = enemyAttackValues[0] * enemyAttackValues[1];
+      player.health = player.health - totalDamage;
+      alert("You have been hit " + enemyAttackValues[1] + " times for " + enemyAttackValues[0] + " damage each for a total of " + totalDamage + " total damage!");
+      if (player.health <= 0) {
+        alert("You have been defeated! Refresh the browser for redemption!");
+        getEnemyHealth.innerHTML = 'Health: ' + enemy.health;
+        getPlayerHealth.innerHTML = 'Health: 0';
+      } else {
+        getPlayerHealth.innerHTML = 'Health: ' + player.health;
+        // Player attack
+        let playerAttackValues = playerAttack();
+        let totalDamage = playerAttackValues[0] * playerAttackValues[1];
+        enemy.health = enemy.health - totalDamage;
+        alert("You hit " + playerAttackValues[1] + " times for " + playerAttackValues[0] + " damage each for a total of " + totalDamage + " total damage!");
+        if (enemy.health <= 0) {
+          alert("You are victorious! Refresh the browser to slay again!");
+          getEnemyHealth.innerHTML = 'Health: 0';
+          getPlayerHealth.innerHTML = 'Health: ' + player.health;
+        } else {
+          getEnemyHealth.innerHTML = 'Health: ' + enemy.health;
+        }
+      }
+    }
   }
+
 }
